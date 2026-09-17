cca29688d01bd6f7305427c76592a5216c95252d
bpraneethreddy1808-max <b.praneethreddy1808@gmail.com>
2026-09-17 18:39:16 +0530

    Add random ID resource and update S3 bucket name
    Added a random ID resource for bucket suffix and updated the S3 bucket name.

--- main.tf
+++ main.tf
@@ -13,9 +13,18 @@ resource "aws_instance" "foo" {
 }
 
 # S3 Bucket
-resource "aws_s3_bucket" "demo" {
-  bucket = "praneeth-tf-s3-20260917-936481""
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
 
+resource "aws_s3_bucket" "demo" {
+  bucket = "testhydbucket-app-x9k2p7"
+
+  tags = {
+    Environment = "Dev"
+    Name        = "Terraform-S3"
+  }
+}
   tags = {
     Name        = "Terraform-S3"
     Environment = "Dev"
