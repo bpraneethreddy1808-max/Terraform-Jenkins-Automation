27999aa3e90c464ac81ef82232ace812a4ec5baa
bpraneethreddy1808-max <b.praneethreddy1808@gmail.com>
2026-09-17 18:22:29 +0530

    Rename S3 bucket in main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "foo" {
 
 # S3 Bucket
 resource "aws_s3_bucket" "demo" {
-  bucket = "testhydbucket20260910"
+  bucket = "praneeth-tf-s3-20260917-936481""
 
   tags = {
     Name        = "Terraform-S3"
